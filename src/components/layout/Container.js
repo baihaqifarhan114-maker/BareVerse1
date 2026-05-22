5040088048de8792251d57533340946615bfc4f6
Farhan Baihaqi <baihaqifarhan114@gmail.com>
2026-05-22 19:52:46 +0700

    fix(build): set noEmit, gitignore emitted js, remove from index

--- src/components/layout/Container.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { cn } from '@/lib/utils';
-export function Container({ className, ...props }) {
-    return _jsx("div", { className: cn('container-wide', className), ...props });
-}
